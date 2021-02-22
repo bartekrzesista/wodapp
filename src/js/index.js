ab67b5a0857ff7ba171fbcb7209f7659381f8e85
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-02-22 22:02:07 +0100

    added main js functionalities

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,32 @@ import '../scss/main.scss';
 
 import {registerSW} from './pwa.js';
 registerSW();
+
+const counter = document.querySelector(".main__counter");
+const addBtn = document.querySelector(".main__btn-add");
+const deleteBtn = document.querySelector(".main__btn-delete");
+
+let date = new Date().toLocaleString("pl-PL");
+const key = date.slice(0, 10);
+
+if(localStorage.getItem(key) == false) {
+    localStorage.setItem(key, 0);
+}
+else {
+    counter.innerHTML = localStorage.getItem(key);
+}
+
+addBtn.addEventListener("click", () => {
+    counter.innerHTML++;
+    localStorage.setItem(key, counter.innerHTML);
+});
+
+deleteBtn.addEventListener("click", () => {
+    if(counter.innerHTML > 0) {
+        counter.innerHTML--;
+        localStorage.setItem(key, counter.innerHTML);
+    }
+    else console.log("Już jest zero");
+});
+
+
